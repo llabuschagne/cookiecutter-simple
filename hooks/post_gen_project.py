7700da74da023ad1d3cf8374fdf4d5f6fdce5fce
llabuschagne <llabuschagne@alphawave.co.za>
2021-07-07 10:20:43 +0200

    better jina2?

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,9 +1,9 @@
 from github import Github
 
-g = Github("{{github_auth_token}}")
-organization = g.get_organization("{{github_organization}}")
+g = Github('{{ github_auth_token }}')
+organization = g.get_organization('{{ github_organization }}')
 repo = organization.create_repo(
-    "{{remote_repo_name}}",
+    '{{ remote_repo_name }}',
     allow_rebase_merge=True,
     auto_init=False,
     description="",
