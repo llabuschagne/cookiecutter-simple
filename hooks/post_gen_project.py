5f1206f6a580a90f75d8cdf64554bd8356dc5652
llabuschagne <llabuschagne@alphawave.co.za>
2021-07-07 13:44:27 +0200

    catching repo creation failures

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,25 +1,28 @@
 from github import Github
 import git
 
-g = Github('{{cookiecutter.github_auth_token}}')
+try:
+    g = Github('{{cookiecutter.github_auth_token}}')
 
-local_repo = git.Repo.init()
+    local_repo = git.Repo.init()
 
-organization = g.get_organization('{{cookiecutter.github_organization}}')
+    organization = g.get_organization('{{cookiecutter.github_organization}}')
 
-repo = organization.create_repo(
-    '{{cookiecutter.remote_repo_name}}',
-    allow_rebase_merge=True,
-    auto_init=False,
-    description="",
-    has_issues=True,
-    has_projects=False,
-    has_wiki=False,
-    private=True,
-)
-origin = local_repo.create_remote('origin', repo.ssh_url)
-local_repo.index.add(local_repo.untracked_files)
-local_repo.index.commit("Intial Commit By Cookiecutter")
-branch = local_repo.active_branch
-branch.rename("main")
-local_repo.git.push("--set-upstream", origin, 'main')
+    repo = organization.create_repo(
+        '{{cookiecutter.remote_repo_name}}',
+        allow_rebase_merge=True,
+        auto_init=False,
+        description="",
+        has_issues=True,
+        has_projects=False,
+        has_wiki=False,
+        private=True,
+    )
+    origin = local_repo.create_remote('origin', repo.ssh_url)
+    local_repo.index.add(local_repo.untracked_files)
+    local_repo.index.commit("Intial Commit By Cookiecutter")
+    branch = local_repo.active_branch
+    branch.rename("main")
+    local_repo.git.push("--set-upstream", origin, 'main')
+except:
+    print('Repo Creation Failed')
