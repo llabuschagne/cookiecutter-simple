fbf70b26eb12f52cf86934d1f26388f88f0dabc8
llabuschagne <llabuschagne@alphawave.co.za>
2021-07-07 10:16:06 +0200

    adding post_gen hook to create repo

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,14 @@
+from github import Github
+
+g = Github("{{github_auth_token}}")
+organization = g.get_organization("{{github_organization}}")
+repo = organization.create_repo(
+    "{{remote_repo_name}}",
+    allow_rebase_merge=True,
+    auto_init=False,
+    description="",
+    has_issues=True,
+    has_projects=False,
+    has_wiki=False,
+    private=True,
+)
